f7dcd4f74fa1f7e370de7a749eda85fc0c3efd25
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2022-10-18 18:23:01 -0400

    made the polyline border for Indiana

--- PercentCoveredByAmbulences/points.cpp
+++ PercentCoveredByAmbulences/points.cpp
@@ -5,9 +5,6 @@
 #include "calculate_distance.h"
 #include <cmath>
 
-
-#include <iostream>
-
 std::vector<std::vector<bool>> points; // The point map, true if active and false if not active (all the points in Indiana start as true)
 int totalPoints; // The total number of points in Indiana
 
@@ -46,41 +43,57 @@ void fillPoints(bool countPoints)
     {
         totalPoints = 0;
     }
-    // These are the coordinates of corners of rectangles that make up the points in Indiana
-    const std::vector<std::pair<std::pair<double, double>, std::pair<double, double>>> rectangleCorners =
+    // These are the coordinates of vertexes of the polygon that make up the Indiana border
+    const std::vector<std::pair<double, double>> indianaBorder =
     {
-        {{39.105480, -87.523661}, {41.759907, -84.820157}},
-        {{39.056190, -87.571926}, {39.105480, -84.897566}},
-        {{38.792125, -87.495200}, {39.056190, -84.897566}},
-        {{38.468242, -87.495200}, {38.792125, -85.498866}},
-        {{38.288894, -87.730132}, {38.468242, -85.697890}},
-        {{38.198666, -87.834796}, {38.288894, -85.885433}},
-        {{37.999366, -87.910623}, {38.198666, -86.525765}},
-        {{38.016023, -86.190050}, {38.198666, -85.932822}}
+        {41.712028, -87.524126},
+        {41.690771, -87.400318},
+        {41.639333, -87.399395},
+        {41.760356, -86.826046},
+        {41.760173, -84.805966},
+        {39.105683, -84.820450},
+        {38.783325, -84.812752},
+        {38.716382, -85.451102},
+        {38.262720, -85.749565},
+        {37.957369, -86.043145},
+        {38.195959, -86.364906},
+        {37.840126, -86.647414},
+        {37.998914, -86.823140},
+        {37.779567, -87.111361},
+        {37.972504, -87.594365},
+        {37.789831, -88.057587},
+        {38.266599, -87.977350},
+        {38.755578, -87.498621},
+        {39.136105, -87.658967},
+        {39.371112, -87.531690},
+        {41.712028, -87.524126}
     };
 
-    for (auto& rectangleCornerSet : rectangleCorners)
-    {
-        // Extract data
-        const double lowLat = rectangleCornerSet.first.first;
-        const double highLat = rectangleCornerSet.second.first;
-        const double lowLong = rectangleCornerSet.first.second;
-        const double highLong = rectangleCornerSet.second.second;
+    // Make 2 for loops which go from LOWEST_LAT and LOWEST_LONG to HIGHEST_LAT and HIGHEST_LONG
+    // If the point is inside the polyline border, set it to true
 
-        const auto [lowLatIndex, lowLongIndex] = coordToIndex({ lowLat, lowLong });
-        const auto [highLatIndex, highLongIndex] = coordToIndex({ highLat, highLong });
-        
-        // Fill the points to be spaced less than MILES_BETWEEN_POINTS apart
-        for (int i = lowLatIndex; i <= highLatIndex; i++)
-        {
-            for (int j = lowLongIndex; j <= highLongIndex; j++)
-            {
-                if (countPoints)
-                {   
-                    totalPoints++;
-                }
-                points[i][j] = true;
-            }
-        }
-    }
+    //for (auto& rectangleCornerSet : rectangleCorners)
+    //{
+    //    // Extract data
+    //    const double lowLat = rectangleCornerSet.first.first;
+    //    const double highLat = rectangleCornerSet.second.first;
+    //    const double lowLong = rectangleCornerSet.first.second;
+    //    const double highLong = rectangleCornerSet.second.second;
+
+    //    const auto [lowLatIndex, lowLongIndex] = coordToIndex({ lowLat, lowLong });
+    //    const auto [highLatIndex, highLongIndex] = coordToIndex({ highLat, highLong });
+    //    
+    //    // Fill the points to be spaced less than MILES_BETWEEN_POINTS apart
+    //    for (int i = lowLatIndex; i <= highLatIndex; i++)
+    //    {
+    //        for (int j = lowLongIndex; j <= highLongIndex; j++)
+    //        {
+    //            if (countPoints)
+    //            {   
+    //                totalPoints++;
+    //            }
+    //            points[i][j] = true;
+    //        }
+    //    }
+    //}
 }
